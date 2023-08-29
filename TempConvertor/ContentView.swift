2dcf9ca0e31e94f5e5f1322a1fcac543e272478d
Maria Sandu <mariafilimon@gmail.com>
2023-08-29 19:55:47 +0300

    Implement logic

--- TempConvertor/ContentView.swift
+++ TempConvertor/ContentView.swift
@@ -8,32 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var baseTemp = 0.0
-    @State private var scaleSelection = "Celsius"
+    @State private var baseTemp: Double = 0.0
+    @State private var scaleSelection = "Fahrenheit"
     @FocusState private var tempIsFocused: Bool
     
-    let tempScale = ["Celsius", "Fahrenheit", "Kelvin"]
+    let tempScale = ["Fahrenheit", "Kelvin"]
+    
+    var convertedTemp: Double {
+        if scaleSelection == "Fahrenheit" {
+            return baseTemp * 1.8 + 32
+        }
+        else {
+            return baseTemp +  273.15
+        }
+    }
     
     var body: some View {
         NavigationView {
             Form {
-                TextField("Temperature", value: $baseTemp, format: .number)
-                    .keyboardType(.decimalPad)
-                    .focused($tempIsFocused)
                 
                 Section {
-                    
-                    Picker("Initial scale", selection: $scaleSelection) {
+                    TextField("Temperature", value: $baseTemp, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($tempIsFocused)
+                } header: {
+                    Text("Celsius temperature")
+                }
+                
+                Section {
+                    Picker("Scale", selection: $scaleSelection) {
                         ForEach(tempScale, id: \.self) {
                             Text("\($0)")
                         }
                     }
                     .pickerStyle(.segmented)
                 } header: {
-                    Text("Select the initial scale")
+                    Text("Select scale")
+                }
+                
+                Section {
+                    Text(convertedTemp, format: .number)
+                } header: {
+                    Text("Converted temperature from celsius")
+                }
+                
+            }
+            .navigationTitle("Temp Convertor")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+
+                    Button("Done") {
+                        tempIsFocused = false
+                    }
                 }
             }
         }
+        
     }
     
 }
