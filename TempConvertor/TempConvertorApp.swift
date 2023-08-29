da74b800766a889999042db6e369923e25fd7bcc
Maria Sandu <mariafilimon@gmail.com>
2023-08-29 15:04:19 +0300

    Initial Commit

---
+++ TempConvertor/TempConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvertorApp.swift
+//  TempConvertor
+//
+//  Created by Maria Sandu on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TempConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
