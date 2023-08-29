91d7459249365198691930ac86679dbe1389b3b1
Maria Sandu <mariafilimon@gmail.com>
2023-08-29 15:49:35 +0300

    Initial scale selector

--- TempConvertor/ContentView.swift
+++ TempConvertor/ContentView.swift
@@ -8,27 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    let scales = ["Celsius", "Fahrenheit", "Kelvin"]
-
+    @State private var baseTemp = 0.0
+    @State private var scaleSelection = "Celsius"
+    @FocusState private var tempIsFocused: Bool
+    
+    let tempScale = ["Celsius", "Fahrenheit", "Kelvin"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                TextField("Temperature", value: $baseTemp, format: .number)
+                    .keyboardType(.decimalPad)
+                    .focused($tempIsFocused)
+                
+                Section {
+                    
+                    Picker("Initial scale", selection: $scaleSelection) {
+                        ForEach(tempScale, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Select the initial scale")
+                }
+            }
         }
-        .padding()
     }
+    
 }
+    
+  
 
 
 
 
 
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+
